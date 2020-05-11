d6cf2911535d0a1d999fe2dae38f3d6fd60d313a
Mawena <gamligocharles@gmail.com>
2020-05-11 15:55:56 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Des petits scripts maison pour améliorer mes expériences utilisateur
---
+++ construct.py
@@ -0,0 +1,18 @@
+import os
+for i in range(1, 5):
+	os.system("mkdir Partie0"+str(i))
+	for j in range(1, 6):
+		os.system("touch Partie0"+str(i)+"/"+str(j)+"-.txt")
+		os.system('echo """'+"""Partie0"""+str(i)+""":
+Chapitre0"""+str(j)+""":
+
+						/1/-Notes importantes:
+#
+
+						/2/-Supléments et infos:
+-
+
+										#BY MAWENA
+
+"""+'""" >> '+"Partie0"+str(i)+"/"+str(j)+"-.txt")
+
